c9bb0bce20c649151640e8cfdbd00a04502b0cae
daniel-idowu-01 <danielidowu414@gmail.com>
2024-03-14 23:56:33 +0100

    added browse by category

---
+++ app/components/BrowseByCategory.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa6";
+
+const BrowseByCategory = () => {
+  return (
+    <main className="m-10 md:py-5 border-b">
+      <p className="border-l-[15px] border-[#DB4444] text-[#DB4444] pl-3 py-1.5 text-xs">
+        Categories
+      </p>
+      <div className="flex flex-col md:flex-row justify-between mt-3">
+        <article className="flex flex-col md:flex-row md:items-center gap-3 md:gap-10">
+          <p className="text-2xl md:text-3xl font-semibold">
+            Browse By Category
+          </p>
+        </article>
+
+        <article className="hidden md:flex items-center gap-2">
+          <span className="p-3 rounded-full bg-[#F5F5F5]">
+            <FaArrowLeft />
+          </span>
+          <span className="p-3 rounded-full bg-[#F5F5F5]">
+            <FaArrowRight />
+          </span>
+        </article>
+      </div>
+
+      {/*  */}
+      <div>
+        
+      </div>
+    </main>
+  );
+};
+
+export default BrowseByCategory;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import FlashSales from "./components/FlashSales";
+import BrowseByCategory from "./components/BrowseByCategory";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <FlashSales />
+      <BrowseByCategory />
     </main>
   );
 }
