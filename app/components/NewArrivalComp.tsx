bc89dfeaf84754251703814ad912e9309e215517
daniel-idowu-01 <danielidowu414@gmail.com>
2024-03-21 12:57:51 +0100

    created grid template 2

--- app/components/NewArrivalComp.tsx
+++ app/components/NewArrivalComp.tsx
@@ -3,37 +3,45 @@ import NewArrivalCompTemplate from "./NewArrivalCompTemplate";
 
 const NewArrivalComp = () => {
   return (
-    <main className="flex gap-5">
-      <section className="w-[40%]">
+    <main className="mt-5 md:flex gap-5">
+      <section className="w-full md:w-[40%]">
         <NewArrivalCompTemplate
           src="/images/playstation.svg"
           width={500}
           height={500}
+          title="Play Station 5"
+          text="Black and White version of the PS5 coming out on sale"
         />
       </section>
 
-      <section className="w-[60%] grid grid-cols-3 gap-5">
-        <article className="col-span-3 w-full">
+      <section className="md:w-[60%] grid md:grid-cols-3 gap-5 mt-5 md:mt-0">
+        <article className="md:col-span-3 w-full">
           <NewArrivalCompTemplate
             src="/images/women_hat.svg"
             width={500}
             height={500}
+            title="Women's Collections"
+            text="Featured woman collections that give you another vibe"
           />
         </article>
 
-        <article className="">
+        <article className="md:col-span-1 w-full">
           <NewArrivalCompTemplate
             src="/images/speakers_2.svg"
-            width={250}
+            width={300}
             height={500}
+            title="Speakers"
+            text="Amazon wireless speakers"
           />
         </article>
 
-        <article>
+        <article className="md:col-span-2 w-full">
           <NewArrivalCompTemplate
             src="/images/perfume.svg"
-            width={250}
+            width={300}
             height={500}
+            title="Perfume"
+            text="GUCCI INTENSE OUD EDP"
           />
         </article>
       </section>
--- app/components/NewArrivalCompTemplate.tsx
+++ app/components/NewArrivalCompTemplate.tsx
@@ -6,24 +6,23 @@ interface NewArrivalCompTemplateProp {
   width: number;
   height: number;
   src: string;
+  title: string;
+  text: string;
 }
 
 const NewArrivalCompTemplate: React.FC<NewArrivalCompTemplateProp> = ({
   width,
   height,
   src,
+  title,
+  text,
 }) => {
   return (
-    <main className="relative bg-black w-fit rounded-md">
-      <Image
-        src={src}
-        alt=""
-        width={width}
-        height={height}
-      />
+    <main className="relative bg-[#0D0D0D] w-full h-full rounded-md">
+      <Image src={src} alt="" width={width} height={height} />
       <article className="w-1/2 absolute bottom-7 left-7 flex flex-col gap-3 text-white">
-        <p className="text-2xl font-semibold">Play Station 5</p>
-        <p>Black and White version of the PS5 coming out on sale</p>
+        <p className="text-2xl font-semibold">{title}</p>
+        <p>{text}</p>
         <ShopNow />
       </article>
     </main>
