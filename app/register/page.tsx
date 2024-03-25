85cfc1025fa129f0efa700fb83c00b2e14982eda
daniel-idowu-01 <danielidowu414@gmail.com>
2024-03-25 23:18:58 +0100

    created register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 
-const Login = () => {
+const Register = () => {
   return (
     <main className="flex items-center justify-between mt-10 mb-20">
       <section className="hidden md:flex justify-center w-[50%]">
@@ -19,10 +19,22 @@ const Login = () => {
       <section className="flex justify-center w-full md:w-[50%]">
         <article>
           <div>
-            <p className="text-3xl md:text-4xl">Login to Exclusive</p>
+            <p className="text-3xl md:text-4xl">Create an account</p>
             <p className="text-sm mt-2">Enter your details below</p>
           </div>
           <form className="flex max-w-md flex-col gap-4 mt-10">
+            <article>
+              <div className="mb-2 block">
+                <Label htmlFor="name" value="Your name" />
+              </div>
+              <TextInput
+                id="name"
+                type="text"
+                placeholder="John Doe"
+                className="focus:ring-transparent w-full"
+                required
+              />
+            </article>
             <article>
               <div className="mb-2 block">
                 <Label htmlFor="email" value="Your email" />
@@ -46,14 +58,14 @@ const Login = () => {
               <Label htmlFor="remember">Remember me</Label>
             </article>
             <Button type="submit" className="bg-[#DB4444]">
-              Login
+              Create an account
             </Button>
           </form>
 
           <p className="text-center text-sm my-5">
-            Don't have an account?{" "}
-            <Link href="/register" className="hover:underline">
-              Register
+            Already have an account?{" "}
+            <Link href="/login" className="hover:underline">
+              Login
             </Link>
           </p>
         </article>
@@ -62,4 +74,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
